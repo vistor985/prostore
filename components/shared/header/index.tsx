049ca326b9a24123004ee38072905d8d0fe8817d
LJM <1743390732@qq.com>
2025-06-19 17:09:16 +0800

    06-theme-mode-toggle

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,9 @@
 import { ShoppingCart, UserIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
-
 import { Button } from '@/components/ui/button';
 import { APP_NAME } from '@/lib/constants';
+import ModeToggle from './mode-toggle';
 
 const Header = () => {
   return (
@@ -24,6 +24,7 @@ const Header = () => {
           </Link>
         </div>
         <div className="space-x-2">
+          <ModeToggle />
           <Button asChild variant="ghost">
             <Link href="/cart">
               <ShoppingCart />
