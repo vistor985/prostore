f75ee39402c03d2dbb7e429aaf81c39ffe4861e4
LJM <1743390732@qq.com>
2025-06-20 15:14:37 +0800

    02-11-product-price-component

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import ProductPrice from './product-price';
 
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
 
@@ -26,7 +27,7 @@ const ProductCard = ({ product }: { product: any }) => {
         <div className="flex-between gap-4">
           <p>{product.rating} stars</p>
           {product.stock > 0 ? (
-            <p className="font-bold">${product.price}</p>
+            <ProductPrice value={Number(product.price)} />
           ) : (
             <p className="text-destructive">Out of Stock</p>
           )}
