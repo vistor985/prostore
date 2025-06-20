c56fe7cb823ee7e973b4ad874a30d39b361a75b8
LJM <1743390732@qq.com>
2025-06-20 11:38:39 +0800

    02-09-list-sample-products

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,16 @@
-const Homepage = () => {
-  return <>Prostore</>;
-};
+import ProductList from '@/components/shared/product/product-list';
+import sampleData from '@/db/sample-data';
 
-export default Homepage;
+const HomePage = () => {
+  return (
+    <div className="space-y-8">
+      <h2 className="h2-bold">Latest Products</h2>
+      <ProductList
+        title="Newest Arrivals"
+        data={sampleData.products}
+        limit={4}
+      />
+    </div>
+  );
+};
+export default HomePage;
