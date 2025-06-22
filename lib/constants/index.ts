075cd36005bbcf29b94b0b59285fe6534fccd62d
LJM <1743390732@qq.com>
2025-06-22 09:58:01 +0800

    03-05-load-products-from-database

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -4,3 +4,5 @@ export const APP_DESCRIPTION =
   'A modern e-commerce platform built with Next.js and TypeScript';
 export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
+export const LATEST_PRODUCTS_LIMIT =
+  Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
