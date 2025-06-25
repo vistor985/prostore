4398eb6666a66c45fda29d397318779f85006167
LJM <1743390732@qq.com>
2025-06-25 11:55:03 +0800

    04-12-sign-up-schema-action

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,4 +1,4 @@
-import { EllipsisVertical, ShoppingCart, UserIcon } from 'lucide-react';
+import { EllipsisVertical, ShoppingCart } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import {
   Sheet,
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,7 +1,9 @@
 'use server';
 
 import { signIn, signOut } from '@/auth';
-import { signInFormSchema } from '../validators';
+import { signInFormSchema, signUpFormSchema } from '../validators';
+import { hashSync } from 'bcrypt-ts-edge';
+import { prisma } from '@/db/prisma';
 
 // Sign in the user with credentials
 export async function signInWithCredentials(
@@ -31,4 +33,45 @@ export async function signInWithCredentials(
 // Sign the user out
 export async function signOutUser() {
   await signOut();
+}
+
+// Register a new user
+export async function signUpUser(prevState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get('name'),
+      email: formData.get('email'),
+      confirmPassword: formData.get('confirmPassword'),
+      password: formData.get('password'),
+    });
+
+    const plainPassword = user.password;
+
+    user.password = hashSync(user.password, 10);
+
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      },
+    });
+
+    await signIn('credentials', {
+      email: user.email,
+      password: plainPassword,
+    });
+
+    return { success: true, message: 'User created successfully' };
+  } catch (error) {
+    // 如果错误是一个重定向错误，则重新抛出
+    if (error instanceof Error && error.name === 'RedirectError') {
+      throw error;
+    }
+
+    return {
+      success: false,
+      message: 'Something went wrong',
+    };
+  }
 }
